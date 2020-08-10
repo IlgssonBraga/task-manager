d1a5a04883d61321ab1b304683746a0cfde42467
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-08-10 17:26:00 -0300

    Task CRUD

--- src/modules/task/controllers/task.controller.ts
+++ src/modules/task/controllers/task.controller.ts
@@ -1,4 +1,87 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Put,
+  Delete,
+  HttpCode,
+} from '@nestjs/common';
+import { TaskService } from '../services/task.service';
+import { Task } from '../../database/entities/Task';
 
-@Controller('task')
-export class TaskController {}
+export interface Request {
+  title: string;
+  user_id: 'uuid';
+  status: string;
+  description: string;
+}
+
+@Controller('tasks')
+export class TaskController {
+  constructor(private taskService: TaskService) {}
+
+  @Post()
+  async create(
+    @Body() { title, user_id, status, description }: Request,
+  ): Promise<Task> {
+    const user = await this.taskService.createTask({
+      title,
+      user_id,
+      status,
+      description,
+    });
+
+    return user;
+  }
+
+  @Get()
+  async index(): Promise<Task[]> {
+    const tasks = await this.taskService.findAllTasks();
+    return tasks;
+  }
+
+  @Get('/:id')
+  async show(@Param() id: string): Promise<Task> {
+    const task = await this.taskService.findOneTask(id);
+
+    if (!task) {
+      throw new Error('Task not found!');
+    }
+
+    return task;
+  }
+
+  @Put('/:id')
+  async update(
+    @Param() id: 'uuid',
+    @Body() { title, user_id, status, description }: Request,
+  ): Promise<Task> {
+    const checkTask = await this.taskService.findOneTask(id);
+
+    if (!checkTask) {
+      throw new Error('Task not found!');
+    }
+
+    const task = await this.taskService.updateTask(id, {
+      title,
+      user_id,
+      status,
+      description,
+    });
+
+    return task;
+  }
+
+  @Delete('/:id')
+  @HttpCode(204)
+  async delete(@Param() id: 'uuid'): Promise<void> {
+    const checkUser = await this.taskService.findOneTask(id);
+
+    if (!checkUser) {
+      throw new Error('User not found!');
+    }
+    await this.taskService.deleteTask(id);
+  }
+}
--- src/modules/task/services/task.service.ts
+++ src/modules/task/services/task.service.ts
@@ -1,4 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Request } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Task } from '../../database/entities/Task';
+
+export interface Request {
+  title: string;
+  user_id: 'uuid';
+  status: string;
+  description: string;
+}
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  constructor(
+    @InjectRepository(Task) private taskRepository: Repository<Task>,
+  ) {}
+
+  async findAllTasks(): Promise<Task[]> {
+    const tasks = await this.taskRepository.find();
+    return tasks;
+  }
+
+  async findOneTask(id: string): Promise<Task> {
+    const task = await this.taskRepository.findOne(id);
+
+    return task;
+  }
+
+  async createTask({
+    title,
+    user_id,
+    status,
+    description,
+  }: Request): Promise<Task> {
+    const task = this.taskRepository.create({
+      title,
+      user_id,
+      status,
+      description,
+    });
+
+    await this.taskRepository.save(task);
+
+    return task;
+  }
+
+  async updateTask(
+    id: string,
+    { title, user_id, status, description }: Request,
+  ): Promise<Task> {
+    try {
+      const task = new Task();
+      Object.assign(task, { title, user_id, status, description });
+      await this.taskRepository.update(id, task);
+      const taskUpdated = await this.taskRepository.findOne(id);
+      return taskUpdated;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  async deleteTask(id: 'uuid'): Promise<void> {
+    await this.taskRepository.delete(id);
+  }
+}
